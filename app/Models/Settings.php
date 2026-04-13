8147598f01cf937e86fcc764b7ae0f238b344ac5
Bertrand <bertrand.tang.dev@gmail.com>
2026-04-13 14:52:57 +0200

    use fillable on settings model

--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['theme', 'lang'])]
 class Settings extends Model
 {
     protected $fillable = [
